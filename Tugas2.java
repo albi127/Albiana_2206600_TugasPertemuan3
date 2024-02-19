e747d52d8c7d3c124cbf647ea71a65d94093c6a4
albi127 <114424440+albi127@users.noreply.github.com>
2024-02-19 21:47:41 +0700

    Update Tugas2.java

--- Tugas2.java
+++ Tugas2.java
@@ -13,20 +13,29 @@ public class Tugas2 {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+    public static void main(String[] args) { // TODO code application logic here
+        Scanner scanner = new Scanner(System.in);
+
+        // Meminta input dari pengguna untuk nilai rangking dan naikKelas
+        System.out.print("Masukkan rangking anda: ");
+        int rangking = scanner.nextInt();
+
+        System.out.print("Apakah Anda naik kelas? (true/false): ");
+        boolean naikKelas = scanner.nextBoolean();
         
-        System.out.print("Masukkan rangking");
-        int rangking = input.nextInt();
-        
-        if (rangking == 1) {
-            // Jika iya, memberi hadiah
-            System.out.println("Selamat! Anda rangking 1, naik kelas, dan mendapat hadiah.");
+        // Mengecek apakah siswa mendapat peringkat 1 di kelasnya dan naik kelas
+        if (rangking <= 30 && naikKelas) {
+            if(rangking == 1 && naikKelas) {
+                System.out.println("Anda mendapat hadiah karena peringkat 1 dan naik kelas!");
+            } else {
+                System.out.println("Anda peringkat ke- " + rangking);
+                System.out.println("Anda Naik kelas tapi anda belum memenuhi syarat untuk mendapatkan hadiah");
+            }
         } else {
-            System.out.println("Maaf, Anda tidak memenuhi syarat untuk mendapatkan hadiah.");
+            System.out.println("mohon maaf anda belum naik kelas");
         }
-        
-        input.close();
+        scanner.close();
     }
-} 
+    
+}
   
